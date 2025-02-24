5c3d32a5daa750aecac9cd36660476be2f51d6ef
ignis <maildevcom+1@gmail.com>
2025-02-23 23:38:49 -0500

    feat: - add ssh outputs;

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "public_ip" {
   description = "The public IP of the EC2 instance"
   value       = aws_instance.my_ec2.public_ip
 }
+
+output "ssh_command" {
+  description = "SSH command to connect to the instance. Update the key file path as needed."
+  value       = "ssh -i ~/.ssh/id_rsa_github ubuntu@${aws_instance.my_ec2.public_ip}"
+}
