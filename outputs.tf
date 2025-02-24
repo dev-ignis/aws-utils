828c5f95108e53897da8623b81346354df748b1d
ignis <maildevcom+1@gmail.com>
2025-02-24 01:07:05 -0500

    feat: - multiple ec2(s);

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
-output "instance_id" {
-  description = "The ID of the EC2 instance"
-  value       = aws_instance.my_ec2.id
+output "instance_ids" {
+  description = "The IDs of the EC2 instances"
+  value       = aws_instance.my_ec2[*].id
 }
 
-output "public_ip" {
-  description = "The public IP of the EC2 instance"
-  value       = aws_instance.my_ec2.public_ip
+output "public_ips" {
+  description = "The public IPs of the EC2 instances"
+  value       = aws_instance.my_ec2[*].public_ip
 }
 
-output "ssh_command" {
-  description = "SSH command to connect to the instance. Update the key file path as needed."
-  value       = "ssh -i ~/.ssh/id_rsa_github ubuntu@${aws_instance.my_ec2.public_ip}"
+output "ssh_commands" {
+  description = "SSH commands to connect to the instances. Update the key file path as needed."
+  value       = [for ip in aws_instance.my_ec2[*].public_ip : "ssh -i ~/.ssh/id_rsa_github ubuntu@${ip}"]
 }
 
 output "load_balancer_dns" {
