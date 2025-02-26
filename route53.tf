f7ee6b4b67b44029f3bac86a15738894e4d4af75
ignis <maildevcom+1@gmail.com>
2025-02-25 23:23:12 -0500

    feat: - alb reference;

--- route53.tf
+++ route53.tf
@@ -1,34 +1,23 @@
-resource "aws_route53_zone" "this" {
-  count = var.create_hosted_zone ? 1 : 0
-  name  = var.hosted_zone_name
-}
-
-locals {
-  zone_id = var.create_hosted_zone ? aws_route53_zone.this[0].id : var.route53_zone_id
-}
-
-# Production API endpoint
 resource "aws_route53_record" "api_production" {
-  zone_id = local.zone_id
+  zone_id = var.route53_zone_id
   name    = var.prod_api_dns_name
   type    = "A"
 
   alias {
-    name                   = aws_lb.app_lb.dns_name
-    zone_id                = aws_lb.app_lb.zone_id
+    name                   = alb.app_lb.dns_name
+    zone_id                = alb.app_lb.zone_id
     evaluate_target_health = true
   }
 }
 
-# Staging API endpoint using the variable
 resource "aws_route53_record" "api_staging" {
-  zone_id = local.zone_id
+  zone_id = var.route53_zone_id
   name    = var.staging_api_dns_name
   type    = "A"
 
   alias {
-    name                   = aws_lb.app_lb.dns_name
-    zone_id                = aws_lb.app_lb.zone_id
+    name                   = alb.app_lb.dns_name
+    zone_id                = alb.app_lb.zone_id
     evaluate_target_health = true
   }
 }
