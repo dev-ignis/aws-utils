6088cf0181c0ffc2764e5f53536f5c88a07e6122
ignis <maildevcom+1@gmail.com>
2025-03-15 22:05:38 -0400

    feat: - dynamodb module ouputs;

--- outputs.tf
+++ outputs.tf
@@ -36,3 +36,14 @@ output "route53_records" {
     api_staging    = (var.skip_route53 || length(aws_route53_record.api_staging) == 0) ? null : aws_route53_record.api_staging[0].name,
   }
 }
+
+# dynamodb module outputs
+output "table_name" {
+  description = "The name of the DynamoDB table"
+  value       = module.dynamodb.table_name
+}
+
+output "table_arn" {
+  description = "The ARN of the DynamoDB table"
+  value       = module.dynamodb.table_arn
+}
