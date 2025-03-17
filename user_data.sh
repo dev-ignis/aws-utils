356293b2703dd1c52270a2718f509470ae4888a9
ignis <maildevcom+1@gmail.com>
2025-03-17 01:46:13 -0400

    feat: - health endpoint;

--- user_data.sh
+++ user_data.sh
@@ -31,8 +31,11 @@ server {
     }
 
     location /health {
-        access_log off;
-        return 200 'OK';
+        proxy_pass http://127.0.0.1:${app_port}/health;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
     }
 
     location /ask-specialist {
