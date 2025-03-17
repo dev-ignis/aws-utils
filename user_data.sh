304e25bd76570d1c8ba9550b7eccb2e89a49b501
ignis <maildevcom+1@gmail.com>
2025-03-17 01:44:27 -0400

    feat: - /users location;

--- user_data.sh
+++ user_data.sh
@@ -50,6 +50,14 @@ server {
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
     }
+
+    location /users {
+        proxy_pass http://127.0.0.1:${app_port}/users;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
 }
 EOL
 
