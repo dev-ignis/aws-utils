c3923f0c507ea3bdcfa298adfe8ec8b65d8e2171
ignis <maildevcom+1@gmail.com>
2025-03-24 22:38:30 -0400

    feat: - email GSI;

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,21 +1,24 @@
 resource "aws_dynamodb_table" "this" {
-  name         = var.table_name
-  billing_mode = var.billing_mode
-
-  hash_key     = var.hash_key
+  name           = var.table_name
+  billing_mode   = var.billing_mode
+  hash_key       = var.hash_key
 
   attribute {
     name = var.hash_key
     type = var.hash_key_type
   }
 
-  # Optionally add a range key if provided
-  dynamic "attribute" {
-    for_each = var.range_key != "" ? [var.range_key] : []
-    content {
-      name = var.range_key
-      type = var.range_key_type
-    }
+  # Add an attribute for email
+  attribute {
+    name = "email"
+    type = "S"
+  }
+
+  # Create a Global Secondary Index on the email attribute
+  global_secondary_index {
+    name            = "email-index"
+    hash_key        = "email"
+    projection_type = "ALL"
   }
 
   tags = var.tags
